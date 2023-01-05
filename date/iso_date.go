39938a6bfd56072058cf775ba7c3f72191d0db07
Thodoris Zois <thzois.csd@gmail.com>
2023-01-05 13:41:32 +0200

    feat: ISODate - UnmarshalParam (#67)
    ## Description
    
    Apart from `UnmarshalJSON`, the `ISODate` can also be used as a query
    parameter. This PR introduces the required functionality to unmarshal an
    ISODate defined as a query parameter.
    
    Note: This PR has been released to `v1.40.11-rc.1`

--- date/iso_date.go
+++ date/iso_date.go
@@ -19,10 +19,24 @@ type ISODate struct {
 // function, unmarshalling will fail because the 'yyyy-mm-dd' format cannot be mapped to a
 // time.Time. To avoid this situation, we need to provide a custom unmarshaller, by overriding
 // the original function 'UnmarshalJSON' so that Golang knows what to do when it encounters
-// a type of an ISODate which tries to be mapped to a time.Time
+// a type of ISODate which tries to be mapped to a time.Time
 func (d *ISODate) UnmarshalJSON(b []byte) (err error) {
 	raw := string(b)
 	t, err := time.Parse(ISODateFmt, raw[1:len(raw)-1])
+	if err != nil {
+		return err
+	}
+
+	d.Time = t
+	return
+}
+
+// UnmarshalParam converts an incoming date string, e.g. 2022-01-01, in a time.Time object
+//
+// The function is similar to UnmarshalJSON but this can be used in order for query parameters
+// and form values: https://echo.labstack.com/guide/request/#custom-binder
+func (d *ISODate) UnmarshalParam(p string) (err error) {
+	t, err := time.Parse(ISODateFmt, p)
 	if err != nil {
 		return err
 	}
