68173c6e6b158038603b15de7ba1f41844c0bdae
Marnick van der Arend <marnick.van.der.arend@ingka.ikea.com>
2026-05-26 16:58:15 +0200

    feat(otel): add StartSpan and Trace span helpers

---
+++ otel/span_test.go
@@ -0,0 +1,164 @@
+// Copyright © 2026 Ingka Holding B.V. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package otel_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	otelSDK "go.opentelemetry.io/otel"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+	otelTrace "go.opentelemetry.io/otel/trace"
+
+	feotel "github.com/ingka-group/fastecho/otel"
+)
+
+func setGlobalTPForSpan(tp otelTrace.TracerProvider) otelTrace.TracerProvider {
+	prev := otelSDK.GetTracerProvider()
+	otelSDK.SetTracerProvider(tp)
+	return prev
+}
+
+func TestStartSpan(t *testing.T) {
+	exp := tracetest.NewInMemoryExporter()
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(exp))
+	defer func() { _ = tp.Shutdown(context.Background()) }()
+
+	prev := setGlobalTPForSpan(tp)
+	defer setGlobalTPForSpan(prev)
+
+	tests := map[string]struct {
+		ctx context.Context
+	}{
+		"creates span with global tracer": {
+			ctx: context.Background(),
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctx, span := feotel.StartSpan(tc.ctx)
+			defer span.End()
+
+			require.NotNil(t, span)
+			require.NotNil(t, ctx)
+			assert.NotPanics(t, func() { span.End() })
+		})
+	}
+}
+
+func TestStartSpan_AutoDiscoversCallerName(t *testing.T) {
+	exp := tracetest.NewInMemoryExporter()
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(exp))
+	defer func() { _ = tp.Shutdown(context.Background()) }()
+
+	prev := setGlobalTPForSpan(tp)
+	defer setGlobalTPForSpan(prev)
+
+	_, span := feotel.StartSpan(context.Background())
+	span.End()
+
+	spans := exp.GetSpans()
+	require.Len(t, spans, 1)
+	assert.Contains(t, spans[0].Name, "TestStartSpan_AutoDiscoversCallerName")
+}
+
+func TestStartSpan_ChildAttachesToParent(t *testing.T) {
+	exp := tracetest.NewInMemoryExporter()
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(exp))
+	defer func() { _ = tp.Shutdown(context.Background()) }()
+
+	prev := setGlobalTPForSpan(tp)
+	defer setGlobalTPForSpan(prev)
+
+	tracer := tp.Tracer("test")
+	ctx, parent := tracer.Start(context.Background(), "parent")
+
+	_, child := feotel.StartSpan(ctx)
+	child.End()
+	parent.End()
+
+	spans := exp.GetSpans()
+	require.Len(t, spans, 2)
+
+	childSpan := spans[0]
+	parentSpan := spans[1]
+
+	assert.Contains(t, childSpan.Name, "TestStartSpan_ChildAttachesToParent")
+	assert.Equal(t, "parent", parentSpan.Name)
+	assert.Equal(t, parentSpan.SpanContext.SpanID(), childSpan.Parent.SpanID())
+}
+
+func TestStartSpan_NoopWhenNoGlobalTracer(t *testing.T) {
+	ctx, span := feotel.StartSpan(context.Background())
+	defer span.End()
+
+	require.NotNil(t, ctx)
+	require.NotNil(t, span)
+	assert.NotPanics(t, func() { span.End() })
+}
+
+func TestTrace(t *testing.T) {
+	exp := tracetest.NewInMemoryExporter()
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(exp))
+	defer func() { _ = tp.Shutdown(context.Background()) }()
+
+	prev := setGlobalTPForSpan(tp)
+	defer setGlobalTPForSpan(prev)
+
+	var called bool
+	feotel.Trace(context.Background(), "heavy-algorithm", func() {
+		called = true
+	})
+
+	assert.True(t, called, "wrapped function should be called")
+
+	spans := exp.GetSpans()
+	require.Len(t, spans, 1)
+	assert.Equal(t, "heavy-algorithm", spans[0].Name)
+}
+
+func TestTrace_ChildAttachesToParent(t *testing.T) {
+	exp := tracetest.NewInMemoryExporter()
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(exp))
+	defer func() { _ = tp.Shutdown(context.Background()) }()
+
+	prev := setGlobalTPForSpan(tp)
+	defer setGlobalTPForSpan(prev)
+
+	tracer := tp.Tracer("test")
+	ctx, parent := tracer.Start(context.Background(), "parent")
+
+	feotel.Trace(ctx, "child-work", func() {})
+	parent.End()
+
+	spans := exp.GetSpans()
+	require.Len(t, spans, 2)
+	assert.Equal(t, "child-work", spans[0].Name)
+	assert.Equal(t, spans[1].SpanContext.SpanID(), spans[0].Parent.SpanID())
+}
+
+func TestTrace_NoopWhenNoGlobalTracer(t *testing.T) {
+	var called bool
+	assert.NotPanics(t, func() {
+		feotel.Trace(context.Background(), "noop-work", func() {
+			called = true
+		})
+	})
+	assert.True(t, called, "function should still execute even without tracing")
+}
