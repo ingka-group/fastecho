3b5bdd67d46192a74afc970291d210299c82fb8e
Marnick van der Arend <marnick.van.der.arend@ingka.ikea.com>
2026-05-26 10:46:11 +0200

    feat(context): deprecate ServiceContext API, add fctx interop to old middleware

--- context/context.go
+++ context/context.go
@@ -1,10 +1,10 @@
-// Copyright © 2024 Ingka Holding B.V. All Rights Reserved.
+// Copyright © 2026 Ingka Holding B.V. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// 	  http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,20 +18,21 @@ import (
 	"github.com/labstack/echo/v4"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
+
+	"github.com/ingka-group/fastecho/fctx"
 )
 
-// ServiceContext contains the echo.Context and custom properties vital for a microservice.
+// Deprecated: Use [fctx.From] and [fctx.Logger]/[fctx.Tracer]/[fctx.RequestID].
+// For shared service configuration, use constructor injection.
 type ServiceContext[T any] struct {
 	echo.Context
-	ZapLogger *zap.Logger
-	Tracer    *trace.Tracer
-	// Props would be shared across all requests in the service
-	Props T
-	// RequestProps values are unique to each requests
+	ZapLogger    *zap.Logger
+	Tracer       *trace.Tracer
+	Props        T
 	RequestProps map[string]interface{}
 }
 
-// BindValidate binds the data to the given interface and validates the input given using validator/10.
+// Deprecated: Use [fastecho.BindValidate] instead.
 func (c *ServiceContext[T]) BindValidate(i interface{}) error {
 	if err := c.Bind(i); err != nil {
 		return err
@@ -44,32 +45,63 @@ func (c *ServiceContext[T]) BindValidate(i interface{}) error {
 	return nil
 }
 
-// GetServiceContext returns the ServiceContext from echo.Context.
+// Deprecated: Use [fctx.From] plus [fctx.Logger]/[fctx.Tracer].
+// Under [fctx.Middleware], returns a best-effort shim from fctx keys.
 func GetServiceContext[T any](ctx echo.Context) *ServiceContext[T] {
-	return ctx.(*ServiceContext[T])
+	if sctx, ok := ctx.(*ServiceContext[T]); ok {
+		return sctx
+	}
+
+	// Compatibility shim for new middleware
+	reqCtx := ctx.Request().Context()
+	var zero T
+	tracer := fctx.Tracer(reqCtx)
+	return &ServiceContext[T]{
+		Context:      ctx,
+		ZapLogger:    fctx.Logger(reqCtx),
+		Tracer:       &tracer,
+		Props:        zero,
+		RequestProps: map[string]interface{}{},
+	}
 }
 
-// ServiceContextMiddleware injects objects to echo.Context.
+// Deprecated: Use [fctx.Middleware] instead.
 func ServiceContextMiddleware[T any](logger *zap.Logger, tracer *trace.Tracer, props T) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
 			req := ctx.Request()
+			reqCtx := req.Context()
 
-			spanCtx := trace.SpanContextFromContext(req.Context())
-			traceId := spanCtx.TraceID().String()
-			spanId := spanCtx.SpanID().String()
+			var fields []zap.Field
+
+			if spanCtx := trace.SpanContextFromContext(reqCtx); spanCtx.IsValid() {
+				fields = append(fields,
+					zap.String("trace_id", spanCtx.TraceID().String()),
+					zap.String("span_id", spanCtx.SpanID().String()),
+				)
+			}
+
+			if reqID := ctx.Response().Header().Get(echo.HeaderXRequestID); reqID != "" {
+				fields = append(fields, zap.String("request_id", reqID))
+				reqCtx = fctx.WithRequestID(reqCtx, reqID)
+			}
+
+			reqLogger := logger.With(fields...)
+
+			reqCtx = fctx.WithLogger(reqCtx, reqLogger)
+			if tracer != nil {
+				reqCtx = fctx.WithTracer(reqCtx, *tracer)
+			}
+
+			ctx.SetRequest(req.WithContext(reqCtx))
 
 			sctx := &ServiceContext[T]{
 				Props:        props,
 				RequestProps: map[string]interface{}{},
 				Context:      ctx,
-				ZapLogger: logger.With(
-					zap.String("trace_id", traceId),
-					zap.String("span_id", spanId),
-				),
+				ZapLogger:    reqLogger,
 			}
 
-			// Add the tracer to the service context
 			if tracer != nil {
 				sctx.Tracer = tracer
 			}
